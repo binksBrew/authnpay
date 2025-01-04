c06ef96be5943533d8c40549b11bbfed3ed3f0b7
Isha Sangpal <143869326+binksBrew@users.noreply.github.com>
2025-01-04 15:18:22 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -179,6 +179,79 @@
 
 
 
+// working
+
+// import React, { useState } from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import HeroWithVideo from "./components/HeroWithVideo";
+// import JoinUs from "./components/JoinUs";
+// import WeAreOnline from "./components/WeAreOnline";
+// import FindUs from "./components/FindUs";
+// import ServiceDetail from "./components/ServiceDetail";
+// import Price from "./components/Price";
+// import SignupPopup from "./components/SignupPopup"; // Import the popup
+// import LiveDemo from "./components/LiveDemo";
+// import TrustedCompanies from "./components/TrustedCompanies";
+// import FAQ from "./components/FAQ";
+// import IntegrateNow from "./components/IntegrateNow";
+// import FeaturesSection from "./components/FeaturesSection";
+// import Support from "./components/Support";
+
+// function App() {
+//   const [showSignup, setShowSignup] = useState(false);
+
+//   const openSignupPopup = () => setShowSignup(true);
+//   const closeSignupPopup = () => setShowSignup(false);
+
+//   return (
+//     <Router>
+//       <div>
+//         <Header openSignupPopup={openSignupPopup} />
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <>
+//                 <HeroWithVideo />
+//                 <JoinUs />
+//                 <WeAreOnline />
+//                 <TrustedCompanies />
+//                 <IntegrateNow />
+//                 <FeaturesSection />
+//                 <FindUs />
+//                 <FAQ />
+//               </>
+//             }
+//           />
+//           <Route path="/services/:serviceId" element={<ServiceDetail />} />
+//           <Route path="/services/:serviceId/pricing" element={<Price />} />
+//           <Route path="/livedemo" element={<LiveDemo />} />
+//           <Route path="/trustedcompanies" element={<TrustedCompanies />} />
+//           <Route path="/faq" element={<FAQ />} />
+//           <Route path="/integratenow" element={<IntegrateNow />} />
+//           <Route path="/featuressection" element={<FeaturesSection />} />
+//           <Route path="/support" element={<Support />} />
+
+//         </Routes>
+//         <Footer />
+
+//         {/* Signup Popup */}
+//         {showSignup && <SignupPopup closePopup={closeSignupPopup} />}
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
 
 
 import React, { useState } from "react";
@@ -210,11 +283,12 @@ function App() {
       <div>
         <Header openSignupPopup={openSignupPopup} />
         <Routes>
+          {/* Home Route */}
           <Route
             path="/"
             element={
               <>
-                <HeroWithVideo />
+                <HeroWithVideo openSignupPopup={openSignupPopup} />
                 <JoinUs />
                 <WeAreOnline />
                 <TrustedCompanies />
@@ -225,16 +299,19 @@ function App() {
               </>
             }
           />
+          {/* Service Details Route */}
           <Route path="/services/:serviceId" element={<ServiceDetail />} />
           <Route path="/services/:serviceId/pricing" element={<Price />} />
+
+          {/* Other Routes */}
           <Route path="/livedemo" element={<LiveDemo />} />
           <Route path="/trustedcompanies" element={<TrustedCompanies />} />
           <Route path="/faq" element={<FAQ />} />
           <Route path="/integratenow" element={<IntegrateNow />} />
           <Route path="/featuressection" element={<FeaturesSection />} />
           <Route path="/support" element={<Support />} />
-
         </Routes>
+
         <Footer />
 
         {/* Signup Popup */}
